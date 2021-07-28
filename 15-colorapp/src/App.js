f90aaa0342cd8dd2bfe5b5c9a9e29f23b7299b2b
Douglas Freitas <dotfreitas@gmail.com>
2021-07-28 20:40:08 -0300

    Add react-router-dom

--- 15-colorapp/src/App.js
+++ 15-colorapp/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Palette from './Components/Palette';
 import seedColors from './Components/seedColors';
 import { generatePalette } from './Components/colorHelpers';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Palette pallete={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette goes here</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>Individual Palette</h1>} />
+      </Switch>
+      // <div>
+      //   <Palette pallete={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
